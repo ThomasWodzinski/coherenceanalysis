38167b06cfb8a972052431efea4673a02f1e1334
ThomasWodzinski <thomas.wodzinski@gmail.com>
2022-01-27 18:00:48 +0000

    create script to geneate notebook for colab

---
+++ build-notebook.py
@@ -0,0 +1,13 @@
+# adpated from https://stackoverflow.com/a/35720002
+
+from IPython.nbformat import v3, v4
+
+with open("main.py") as fpin:
+    text = fpin.read()
+
+nbook = v3.reads_py(text)
+nbook = v4.upgrade(nbook)  # Upgrade v3 to v4
+
+jsonform = v4.writes(nbook) + "\n"
+with open("main.ipynb", "w") as fpout:
+    fpout.write(jsonform)
