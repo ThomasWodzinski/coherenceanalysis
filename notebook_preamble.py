8200c1f1f3491f96d740b18d6c547230c2c50c91
ThomasWodzinski <thomas.wodzinski@gmail.com>
2022-01-27 20:23:16 +0000

    add clone to main and comment out to develop

--- notebook_preamble.py
+++ notebook_preamble.py
@@ -3,7 +3,8 @@
 
 from pathlib import Path  # see https://docs.python.org/3/library/pathlib.html#basic-use
 
-! git clone --branch develop https://github.com/ThomasWodzinski/coherence-analysis.git
+# ! git clone --branch develop https://github.com/ThomasWodzinski/coherence-analysis.git
+! git clone https://github.com/ThomasWodzinski/coherence-analysis.git
 %cd coherence-analysis/
 ! pip install lmfit
 
