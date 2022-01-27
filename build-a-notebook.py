d4584ab95032c85bf75c20d0ac96d0c094169a7b
ThomasWodzinski <thomas.wodzinski@gmail.com>
2022-01-27 20:15:49 +0000

    change the way script and notebook are created

--- build-a-notebook.py
+++ build-a-notebook.py
@@ -2,12 +2,19 @@
 
 from nbformat import v3, v4 
 
+with open("notebook_preamble.py") as fpin:
+    text1 = fpin.read()
+
 with open("main.py") as fpin:
-    text = fpin.read()
+    text2 = fpin.read()
+
+text = text1 + '\n'
+text += text2
+
 
 nbook = v3.reads_py(text)
 nbook = v4.upgrade(nbook)  # Upgrade v3 to v4
 
 jsonform = v4.writes(nbook) + "\n"
-with open("main.ipynb", "w") as fpout:
+with open("coherence-analysis.ipynb", "w") as fpout:
     fpout.write(jsonform)
---
+++ notebook_preamble.py
@@ -0,0 +1,22 @@
+# <codecell>
+# for colab notebook
+
+from pathlib import Path  # see https://docs.python.org/3/library/pathlib.html#basic-use
+
+! git clone --branch develop https://github.com/ThomasWodzinski/coherence-analysis.git
+%cd coherence-analysis/
+! pip install lmfit
+
+## Define paths
+# use data stored in own google drive location
+from google.colab import drive
+
+drive.mount('/content/gdrive', force_remount=True)
+data_dir = Path('/content/gdrive/MyDrive/PhD/coherence/data/')
+useful_dir = Path('/content/gdrive/MyDrive/PhD/coherence/data/useful/')
+bgsubtracted_dir = Path('/content/gdrive/MyDrive/PhD/coherence/data/bgsubtracted/')
+print(useful_dir)
+scratch_dir = Path('/content/gdrive/MyDrive/PhD/coherence/data/scratch_cc/')
+#prebgsubtracted_dir
+#bgsubtracted_dir = Path.joinpath('/content/gdrive/MyDrive/PhD/coherence/data/scratch_cc/','bgsubtracted')
+
