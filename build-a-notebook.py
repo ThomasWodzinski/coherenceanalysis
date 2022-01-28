6532405d8422b593929096112b14cba7d094fe9c
ThomasWodzinski <thomas.wodzinski@gmail.com>
2022-01-28 18:17:19 +0000

    comment out %load magic, not working in colab

--- build-a-notebook.py
+++ build-a-notebook.py
@@ -5,10 +5,10 @@ from nbformat import v3, v4
 with open("notebook_preamble.py") as fpin:
     text1 = fpin.read()
 
-# with open("main.py") as fpin:
-#     text2 = fpin.read()
+with open("main.py") as fpin:
+    text2 = fpin.read()
 
-text2 = '# <codecell>\n%load main.py'
+# text2 = '# <codecell>\n%load main.py'
 
 text = text1 + '\n'
 text += text2
