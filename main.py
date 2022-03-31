f7b33bc4346e7720cdfc79f212c9571113969348
ThomasWodzinski <thomas.wodzinski@gmail.com>
2022-03-31 17:37:43 +0100

    store to csv after each measurement

--- main.py
+++ main.py
@@ -3176,6 +3176,11 @@ def run_over_all_images():
         time_taken = end - start
         time_left = time_taken/i * (len(imageid_profile_fit_widget.options) - i)
         run_over_all_images_statustext_widget.value = str(time_taken) + "|" + str(time_left)
+    
+    df_fits = df0[['timestamp_pulse_id'] + fits_header_list]
+    df_fits_csv_file = df_fits_csv_files_widget.value
+    df_fits.to_csv(df_fits_csv_file)
+
     run_over_all_images_progress_widget.bar_style = 'success'
 
 
